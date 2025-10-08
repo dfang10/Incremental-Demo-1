9629b4ac39ab4515e3f53ae0ddc5861cd633761a
dfang10 <dfang10@ucsc.edu>
2025-10-08 05:22:51 +0000

    added button

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import "./style.css";
 
+let counter: number = 0;
 document.body.innerHTML = `
   <img src="/ado-plush.png" alt="Ado Plush" />
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment">Click Me!</button>
 `;
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.innerHTML = counter.toString();
+});
